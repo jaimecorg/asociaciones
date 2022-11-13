14a3d9c4cb1712838438f13aed83b3d27bc422c0
Jaime Corpas <jaime.corpas.785@estudiante.iesoretania.es>
2022-11-13 19:59:04 +0100

    Creamos getters y setters de empleado

--- src/Entity/Empleado.php
+++ src/Entity/Empleado.php
@@ -39,4 +39,85 @@ class Empleado
      * @var bool
      */
     private $estaActivo;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return Empleado
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param string $apellidos
+     * @return Empleado
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }
+
+    /**
+     * @param \DateTime $fechaNacimiento
+     * @return Empleado
+     */
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEstaActivo()
+    {
+        return $this->estaActivo;
+    }
+
+    /**
+     * @param bool $estaActivo
+     * @return Empleado
+     */
+    public function setEstaActivo($estaActivo)
+    {
+        $this->estaActivo = $estaActivo;
+        return $this;
+    }
+
 }
